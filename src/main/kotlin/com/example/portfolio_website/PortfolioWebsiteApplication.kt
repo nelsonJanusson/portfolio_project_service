0531e511f36d122c2eeb6d6bef12fa2361b0ec3c
nelson <hejnelson@gmail.com>
2025-03-18 18:36:40 +0100

    first git commit

---
+++ src/main/kotlin/com/example/portfolio_website/PortfolioWebsiteApplication.kt
@@ -0,0 +1,11 @@
+package com.example.portfolio_website
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioWebsiteApplication
+
+fun main(args: Array<String>) {
+    runApplication<PortfolioWebsiteApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/portfolio_website/PortfolioWebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.portfolio_website
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
