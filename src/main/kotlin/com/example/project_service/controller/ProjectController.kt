aac321feb8cf9945fc0df23662c52512fbb27e64
nelson <hejnelson@gmail.com>
2025-03-24 13:00:35 +0100

    ad db

--- src/main/kotlin/com/example/project_service/controller/ProjectController.kt
+++ src/main/kotlin/com/example/project_service/controller/ProjectController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(value = ["/api/projects"])
-
 class ProjectController(val projectService: ProjectService) {
 
     @GetMapping
