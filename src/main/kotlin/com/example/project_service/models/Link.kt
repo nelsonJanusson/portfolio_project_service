d5d6dc719ff3c3ece85cc8c9a39de14c04d367d4
nelson <hejnelson@gmail.com>
2025-04-15 22:48:45 +0200

    add maybe postgress

--- src/main/kotlin/com/example/project_service/models/Link.kt
+++ src/main/kotlin/com/example/project_service/models/Link.kt
@@ -1,3 +1,6 @@
 package com.example.project_service.models
 
+import jakarta.persistence.Embeddable
+
+@Embeddable
 data class Link(val name: String, val url: String)
--- src/main/kotlin/com/example/project_service/models/Project.kt
+++ src/main/kotlin/com/example/project_service/models/Project.kt
@@ -1,10 +1,19 @@
 import com.example.project_service.models.Link
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import lombok.NoArgsConstructor
 import java.util.UUID
-
+@Entity
+@Table(name = "users")
 class Project(
-
+    @Id
     val id: UUID = UUID.randomUUID(),
     val title: String,
     val link: Link,
     val description: String,
-    val tags: List<String>)
+    @ElementCollection
+    val tags: List<String>) {
+
+}
